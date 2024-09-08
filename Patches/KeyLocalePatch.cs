da711505c1f66d6595713ddf9206143b8cba54c3
will258012 <125041335+will258012@users.noreply.github.com>
2024-09-08 08:58:53 +0800

    添加对键位文字的修复

---
+++ Patches/KeyLocalePatch.cs
@@ -0,0 +1,34 @@
+﻿using ColossalFramework.Globalization;
+using HarmonyLib;
+using static ColossalFramework.Globalization.Locale;
+
+namespace BoardingTranslationFix.Patches
+{
+    [HarmonyPatch]
+    public static class KeyLocalePatch
+    {
+        [HarmonyPatch(typeof(Locale), nameof(Locale.Get), new[] { typeof(Key) })]
+        public static bool Prefix(ref string __result, Key id)
+        {
+            if (id.m_Key == "LeftBracket")
+            {
+                __result = "[键";
+                return false;
+            }
+            if (id.m_Key == "RightBracket")
+            {
+                __result = "]键";
+                return false;
+            }
+            if (id.m_Key == "Equals")
+            {
+                __result = "=键";
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
+
+
