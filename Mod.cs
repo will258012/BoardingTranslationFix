d0d53b1cb33c1aacad85c1b0e00f47117bc57c8a
Will <125041335+will258012@users.noreply.github.com>
2024-11-16 21:30:47 +0800

    v1.0.3
    - 使用 WillCommons

--- Mod.cs
+++ Mod.cs
@@ -1,49 +1,11 @@
-﻿using BoardingTranslationFix.Utils.Log;
-using ColossalFramework.Globalization;
-using ICities;
-using Snooper.Utils;
-using System.Reflection;
+﻿using WillCommons;
 
 namespace BoardingTranslationFix
 {
-    public class Mod : IUserMod, ILoadingExtension
+    public class Mod : PatcherModBase
     {
-        public string Name => "Boarding Translation Fix v" + ModVersion;
-        public string Description => "中文翻译修正";
-        private const string HarmonyId = "Will258012.BoardingTranslationFix";
-
-        private string ModVersion
-        {
-            get
-            {
-                var assemblyVersion = ModAssembly.GetName().Version;
-                return $"{assemblyVersion.Major}.{assemblyVersion.Minor}.{assemblyVersion.Build}";
-            }
-        }
-        public void OnEnabled() => HarmonyPatcher.PatchOnReady(ModAssembly, HarmonyId);
-
-        public void OnCreated(ILoading loading)
-        {
-
-        }
-        public void OnLevelLoaded(LoadMode mode)
-        {
-            if (LocaleManager.exists && LocaleManager.instance.language != "zh")
-            {
-                Log.Err("Game language is not supported!");
-            }
-        }
-
-        public void OnLevelUnloading()
-        {
-
-        }
-        public void OnReleased()
-        {
-        }
-
-        public void OnDisabled() => HarmonyPatcher.TryUnpatch(HarmonyId);
-        private Assembly ModAssembly => Assembly.GetExecutingAssembly();
-
+        public override string BaseName => "Boarding Translation Fix";
+        public override string Description => "中文翻译修正";
+        public override string HarmonyID => "Will258012.BoardingTranslationFix";
     }
 }
